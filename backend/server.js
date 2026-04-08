edd793df3eab7087a7e478d08552f73a451e5a6d
Gabriel Amarom <gabrielnahmur@gmail.com>
2026-04-08 11:11:38 -0300

    Aumenta limite global de JSON no express para 50mb

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Necessário limite maior para agent push
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Inicializar banco de dados
